f9d90024ec5344c3f9cbe0e70c316635f2f6461d
Sai Premika <saipremikakadiri@gmail.com>
2018-10-06 10:57:46 +0530

    Admin table created

--- 1MV16CS038_48/SRS/src/srs/SRS.java
+++ 1MV16CS038_48/SRS/src/srs/SRS.java
@@ -8,6 +8,9 @@ public class SRS {
 
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
         // TODO code application logic here
+        
+        SqlDao obj = new SqlDao();
+        obj.createTableAdmin();
 
      
     }       
@@ -16,20 +19,44 @@ public class SRS {
         
 }
 
-class Con {
+class SqlDao {
 
     Connection con = null;
 
-    public Con() {
+    public SqlDao() {
 
         try {
             Class.forName("com.mysql.jdbc.Driver");
-            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/srs", "root", "dolly");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/srs", "root", "premi");
             
         } catch (Exception ex) {
             System.out.println(ex);
         }
         
+    }
+    
+    public boolean createTableAdmin() throws SQLException{
+        
+        
+        String sql="CREATE TABLE IF NOT EXISTS ADMIN"
+                + "(NAME VARCHAR(15) NOT NULL,"
+                + "USER_NAME VARCHAR(15),"
+                + "PASSWORD VARCHAR(8) NOT NULL,"
+                + "CONSTRAINT PF_UN PRIMARY KEY(USER_NAME));";
+        
+        Statement st = con.createStatement();
+        st.execute(sql);
+        con.close();
+        return true;
+    }
+    
+    public int createDB() throws SQLException{
+        
+        String sql ="Create database srs";
+        PreparedStatement ps = con.prepareStatement(sql);
+        
+        int n=ps.executeUpdate();
+        return n;
     }
     
 }
