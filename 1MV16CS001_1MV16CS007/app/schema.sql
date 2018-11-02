10ba9eda0726aeaf15174328f15dda77310d79f2
cseas <contact.abhijeetsingh@gmail.com>
2018-11-02 09:04:40 +0530

    Add schema

--- 1MV16CS001_1MV16CS007/app/schema.sql
+++ 1MV16CS001_1MV16CS007/app/schema.sql
@@ -1,3 +1,16 @@
+-- Created by Abhijeet Singh
+-- USN: 1MV16CS001
+
+-- drop table if it exists
+BEGIN
+   EXECUTE IMMEDIATE 'DROP TABLE ' || teacher;
+EXCEPTION
+   WHEN OTHERS THEN
+      IF SQLCODE != -942 THEN
+         RAISE;
+      END IF;
+END;
+
 create table teacher(
 	dept_name varchar(10),
 	f_name varchar(10),
@@ -6,8 +19,8 @@ create table teacher(
 	teacher_id varchar(10),
 	Primary key(teacher_id),
 	foreign key(dept_name)references dept(dept_name)
-
 	);
+
 create table teacher_course(
 	course_id varchar(10),
 	section_id varchar(10),
@@ -18,6 +31,7 @@ create table teacher_course(
 	foreign key (teacher_id) references teacher(teacher_id),
 	foreign key(course_id,section_id,sem,year) references (course_id,section_id,sem,year)
 	);
+
 create table section(
 	building number(2),
 	room_no number(3),
@@ -30,6 +44,7 @@ create table section(
 	foreign key(course_id) references course_info(course_id),
 	foreign key(building,room_no) references classroom(building,room_no)
 	);
+
 create table timeslot(
 	timeslot_id  varchar(10),
 	day char(3),
@@ -38,15 +53,15 @@ create table timeslot(
 	end_hour number(2),
 	end_min number(2),
 	primary key(timeslot_id,day,start_hour,start_min),
-
 	);
+
 create table classroom(
 	capacity number(3),
 	room_no number(3),
 	building number(2),
 	primary key(building,room_no)
-
 	);
+
 create table course_info(
 	course_id varchar(10),
 	dept_name varchar(10),
@@ -56,19 +71,21 @@ create table course_info(
 	primary key (course_id),
 	foreign key(dept_name) references dept(dept_name)
 	);
+
 create table dept(
 	dept_name varchar(10),
 	building number(2),
 	primary key(dept_name)
 	);
+
 create table student(
 	stud_id varchar(10),
 	name varchar(10),
 	dept_name varchar(10),
 	primary key(stud_id),
 	foreign key(dept_name) references dept(dept_name)
-
 	);
+
 create table student_course(
 	stud_id varchar(10),
 	course_id varchar(10),
@@ -80,6 +97,7 @@ create table student_course(
 	foreign key(course_id,section_id,sem,year) references section(course_id,section_id,sem,year),
 	foreign key(stud_id) references student(stud_id)
 	);
+    
 create table local_guardian(
 	teacher_id varchar(10),
 	stud_id varchar(10),
